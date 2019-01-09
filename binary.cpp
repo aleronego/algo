eaa6b4be7e45e924fa7a305c68d7b59d5d6e6e34
Don <aleronego@gmail.com>
2019-01-09 13:58:25 +0200

    Add binary search impl

---
+++ binary.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int binary_search(int num, int arr[])
+{
+    int l = 0;
+    int h = 100;
+    int it = 0;
+    while (l <= h) {
+        ++it;
+        int mid = (h + l) / 2;
+        cout << "it: " << it << " mid: " << mid << endl;
+        if (arr[mid] == num)
+            return mid;
+        else if (arr[mid] < num)
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[101];
+    for (int i = 0; i < 101; ++i) {
+        arr[i] = i;
+    }
+    cout << endl
+         << "Your number: (0-100) ";
+    int num = 0;
+    cin >> num;
+    if (num >= 0 && num <= 100) {
+        cout << endl
+             << binary_search(num, arr) << " found" << endl;
+    } else {
+        cout << endl
+             << "Wrong number" << endl;
+    }
+}
