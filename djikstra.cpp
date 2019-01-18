57514a667823ec2f12660f11d657c442e082e4bb
Don <aleronego@gmail.com>
2019-01-18 20:26:50 +0200

    Add print reverse route

--- djikstra.cpp
+++ djikstra.cpp
@@ -2,39 +2,44 @@
 #include <iostream>
 #include <list>
 #include <vector>
-#include <limits>
 #include <set>
 using namespace std;
-//#define INF 0x3f
-#define INF numeric_limits<int>::max()
+#define INF 0x3f3f3f3f
+ 
 class Graph
 {
-	int V;
+    int V;
     list< pair<int,int> > *adj;
+    set< pair<int,int> > setds;
+    vector<int> dist;
+
+    void djikstra(int);
+    void printDist();
+    void printAdj();
+    void printRoute(int, int);
+
 public:
-	Graph(int);
-	void addEdge(int, int, int);
-	void djikstra(int, int);
+    Graph(int);
+    void addEdge(int, int, int);
+    void searchAndPrint(int, int);
 };
 
 Graph::Graph(int v) : V(v)
 {
-	adj = new list< pair<int,int> >[V];
+    adj = new list< pair<int,int> >[V];
+    dist = vector<int> (V, INF );
 }
 
 void Graph::addEdge(int a, int b, int dist)
 {
-	adj[a].push_back( make_pair(b, dist) );
-	adj[b].push_back( make_pair(a, dist) );
+    adj[a].push_back( make_pair(b, dist) );
+    adj[b].push_back( make_pair(a, dist) );
 }
 
-void Graph::djikstra(int s, int to)
+void Graph::djikstra(int s) 
 {
-	set< pair<int,int> > setds;
-	vector<int> dist(V, numeric_limits<int>::max() );
-
-	setds.insert(make_pair(0, s));
-	dist[s] = 0;
+    setds.insert(make_pair(0, s));
+    dist[s] = 0;
 
     while(!setds.empty())
     {
@@ -57,49 +62,66 @@ void Graph::djikstra(int s, int to)
             }
         }
     }
-    ////////////////////////////////////
+}
+
+void Graph::printDist()
+{
     cout << "V\tDist\n";
     for(int i = 0; i < V; ++i)
-    	cout << i << "\t" << dist[i] << endl;
-    ////////////////////////////////////
+        cout << i << "\t" << dist[i] << endl;
+    cout << endl;
+}
+
+void Graph::printAdj()
+{
     cout << "\nAdj\n";
     for(int i = 0; i < V; ++i)
     {
-    	cout << endl << i << endl;
+        cout << endl << i << endl;
         for(auto it = adj[i].begin(); it != adj[i].end(); ++it)
         {
-    	    cout << (*it).first << " " << (*it).second << endl;
+            cout << (*it).first << " " << (*it).second << endl;
         }
     }
-    ////////////////////////////////////
+    cout << endl;
+}
+
+void Graph::printRoute(int s, int to)
+{
     cout << "Reverse route " << s << " to " << to << " ";
     list<int>queue;
     queue.push_back(to);
     while(!queue.empty())
     {
-    	int temp = queue.back();
-    	queue.pop_back();
+        int temp = queue.back();
+        queue.pop_back();
         int min = temp;
         for(auto it = adj[temp].begin(); it != adj[temp].end(); ++it)
         {
-        	int curr = (*it).first;
+            int curr = (*it).first;
             if(dist[curr] < dist[min])
-            	min = curr; 
+                min = curr; 
         }
 
         if(min!=s)
             queue.push_back(min);
         cout << min << " ";
     }
-    ////////////////////////////////////
+    cout << endl;
 }
+
+void Graph::searchAndPrint(int start, int to)
+{
+    djikstra(start);
+    printRoute(start,to);
+}
+
 
 int main()
 {
+    Graph g(9);
 
-	Graph g(9);
-
-	g.addEdge(0, 1, 4); 
+    g.addEdge(0, 1, 4); 
     g.addEdge(0, 7, 8); 
     g.addEdge(1, 2, 8); 
     g.addEdge(1, 7, 11); 
@@ -114,7 +136,7 @@ int main()
     g.addEdge(6, 8, 6); 
     g.addEdge(7, 8, 7); 
 
-    g.djikstra(0, 4);
+    g.searchAndPrint(0, 4);
 
     return 0;
 }
